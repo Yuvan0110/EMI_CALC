d241efd0c8212c33092ae447e7334ab5ab1cf5ca
Yuvan kumar <yuvankumarayk@gmail.com>
2025-08-27 09:15:19 +0530

    refactored

---
+++ app/src/main/java/com/example/emi_calc_app/viewModelRepository/viewModelRepository.kt
@@ -0,0 +1,86 @@
+package com.example.emi_calc_app.viewModelRepository
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.core.text.isDigitsOnly
+import com.example.emi_calc_app.data.InputState
+import com.example.emi_calc_app.data.TenureUnit
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
+
+class ViewModelRepository {
+    private val _input = mutableStateOf(InputState())
+    val inputState: InputState get() = _input.value
+
+    var startDateTenure by mutableLongStateOf(System.currentTimeMillis())
+
+    fun setStartDate(stDate: Long) {
+        startDateTenure = stDate
+    }
+
+    fun onPrincipalChange(value: String) {
+        if (value.isDigitsOnly() && value.length <= 10) {
+            _input.value = _input.value.copy(principal = value)
+        }
+    }
+
+    fun onInterestChange(value: String) {
+        val num = value.toDoubleOrNull() ?: 0.0
+        if (num in 0.0..100.0 && value.matches(Regex("(^\\d+)?(\\.\\d*)?$"))) {
+            _input.value = _input.value.copy(interest = value)
+        }
+    }
+
+    fun onTenureChange(value: String) {
+        if (value.isDigitsOnly() && value.length <= 4) {
+            _input.value = _input.value.copy(tenure = value)
+        }
+    }
+
+    fun setPrincipal(principal : String) {
+        _input.value = _input.value.copy(principal = principal)
+    }
+
+    fun setInterest(interestRate : String) {
+        _input.value = _input.value.copy(interest = interestRate)
+    }
+
+    fun setTenure(tenure : String) {
+        _input.value = _input.value.copy(tenure = tenure)
+    }
+
+    fun setTenureUnit(unit: TenureUnit) {
+        _input.value = _input.value.copy(tenureUnit = unit)
+    }
+
+    fun getPrincipal() : Double {
+        return _input.value.principal.toDoubleOrNull() ?: 0.0
+    }
+    fun getInterest() : Double{
+        return _input.value.interest.toDoubleOrNull() ?: 0.0
+    }
+    fun getTenure() : Int {
+       return _input.value.tenure.toIntOrNull() ?: 0
+    }
+    fun getTenureUnit() : TenureUnit {
+        return _input.value.tenureUnit
+    }
+
+    fun getStartDate(): LocalDate {
+        return Instant.ofEpochMilli(startDateTenure).atZone(ZoneId.systemDefault()).toLocalDate()
+    }
+
+    fun getMonths(): Int {
+        return when (getTenureUnit()) {
+            TenureUnit.YEARS -> getTenure() * 12
+            TenureUnit.MONTHS -> getTenure()
+        }
+    }
+
+    fun isValid(): Boolean {
+        return inputState.principal.isNotEmpty() && inputState.interest.isNotEmpty() && inputState.tenure.isNotEmpty()
+    }
+}
